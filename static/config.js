60906e9f851b55d828da10516c57a9b50b5941a8
mrw95 <mrw95@users.noreply.github.com>
2026-08-20 10:53:17 +0000

    deploy: 5e5da183ce198ec8bc1500e30d69ab4df144336e

---
+++ static/config.js
@@ -0,0 +1,5 @@
+// === EXCEL SAVE SETUP ===
+const SHEET_URL = 'https://script.google.com/macros/s/AKfycbyU4GZoFuds2NbaNIR_a6zXClEmyxEC8TbPlk-7lzMUFBmNcAjhLP4zNn-HRnw8Di9V/exec';
+
+// Sheet open link — browser eke sheet tab eke address bar URL (optional, results open karanna)
+const RESULTS_SHEET = '';
