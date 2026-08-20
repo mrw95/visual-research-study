b1bcf3a0778014d6fe4081611c2be001a64b5e90
mrw95 <mrw95@users.noreply.github.com>
2026-08-20 12:19:18 +0000

    deploy: 875b6a0f4ac8a819c14d3f77aaf2408934b6bc70

--- static/config.js
+++ static/config.js
@@ -4,7 +4,7 @@
 const SURVEY_URL = 'https://mrw95.github.io/visual-research-study/';
 
 // Excel save — Google Apps Script Web App (NOT library URL!)
-const SHEET_URL = 'https://script.google.com/macros/s/AKfycbyU4GZoFuds2NbaNIR_a6zXClEmyxEC8TbPlk-7lzMUFBmNcAjhLP4zNn-HRnw8Di9V/exec';
+const SHEET_URL = 'https://script.google.com/macros/s/AKfycbwHo2lMNSP1sBV_s7HFW83-Bn_RwacptGEfXJX56sLpoHWaIub4NULajSjuO9pPu5F2/exec';
 
 // Results balanna — Google Sheet browser link (optional)
 const RESULTS_SHEET = '';
